e2f4c81960285dce96f7af27ac907f2b02e01752
Jamie <j.bonner6992@gmail.com>
2026-05-28 15:46:44 +0100

    Add game intro method

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,32 @@ class Game
   end
 
   def intro
-    
+    puts "###############################################################"
+    puts "#            Welcome to Escape from the ISS Ravana            #"
+    puts "#-------------------------------------------------------------#"
+    puts "#                                                             #"
+    puts "#     Escape from ISS Ravana is a text based survival game    #"
+    puts "#     where the aim of the game is to collect all the items   #"
+    puts "#                and escape from the ship.                    #"
+    puts "#     But keep your wits about you as you explore the ship    #"
+    puts "#            as you may not be the only one left!             #"
+    puts "#                                                             #"
+    puts "###############################################################"
+    puts "#                      Instructions                           #"
+    puts "#-------------------------------------------------------------#"
+    puts "#                                                             #"
+    puts "#                     Type Commands :                         #"
+    puts "#        To move between rooms type go and the direction:     #"
+    puts "#               'Go' - 'Left', 'Right', 'Up', 'Down'          #"
+    puts "#                                                             #"
+    puts "#                To get and item in a room type:              #"
+    puts "#               'Get' and the items name 'Spanner'            #"
+    puts "#                                                             #"
+    puts "#               Type 'Help' to see the games controls         #"
+    puts "#               Type 'Restart' to Restart the game            #"
+    puts "#                 Type 'Exit' to exit the game                #"
+    puts "#                                                             #"
+    puts "###############################################################"
   end
 
   def player
